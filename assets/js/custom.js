c7510960134c07502df197eaa36e80fe52308f36
azriq <azriqfawani.jamaludin@bizedge.com>
2017-07-26 10:51:29 +0800

    initial add files

---
+++ assets/js/custom.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+ $('span#b1').click(function(e){
+    $('#fullscreen-1').toggleClass('expanded'); 
+ });
+
+  $('span#b2').click(function(e){
+    $('#fullscreen-2').toggleClass('expanded'); 
+ });
+
+  $('span#b3').click(function(e){
+    $('#fullscreen-3').toggleClass('expanded'); 
+ });
+
+  $('span#b4').click(function(e){
+    $('#fullscreen-4').toggleClass('expanded'); 
+ });
+
+  $('.expanded').toggleClass()
+
+
+});
+
